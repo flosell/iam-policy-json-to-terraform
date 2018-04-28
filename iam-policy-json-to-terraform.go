98da486cd521f37c5f49ed0462467fdad4e8aeba
Florian Sellmayr <florian.sellmayr@gmail.com>
2018-04-28 14:42:06 +0700

    Try to encode some basic policy document

--- iam-policy-json-to-terraform.go
+++ iam-policy-json-to-terraform.go
@@ -1,7 +1,27 @@
 package main
 
 import "fmt"
+import (
+	"github.com/hashicorp/hcl/hcl/printer"
+	"os"
+	"github.com/flosell/iam-policy-json-to-terraform/encoder"
+)
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+
+	policy_document := encoder.PolicyDocument{
+		Name: "deny_access_without_mfa",
+		Statements: encoder.StatementList{
+			Statements: []*encoder.Statement{
+				&encoder.Statement{
+					Sid: "BlockMostAccessUnlessSignedInWithMFA",
+
+				},
+			},
+		},
+	}
+
+	printer.Fprint(os.Stdout, policy_document.Encode())
+	fmt.Printf("\nHello, world.\n")
+
 }
