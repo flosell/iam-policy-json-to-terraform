4c843fa48143ec0a6d28689b2ded1bd3372c14e7
Florian Sellmayr <florian.sellmayr@gmail.com>
2021-08-29 14:54:36 +0700

    Add test for web wrapper (#24)

---
+++ web/web_test.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestWebWrapperConvertsString(t *testing.T) {
+	result := ConvertString("someName", "{}")
+	assert.Equal(t, "data \"aws_iam_policy_document\" \"someName\" {}\n", result)
+}
+
+func TestWebWrapperPanicsIfThingsCanBeParsedSoWeGetANiceJavaScriptException(t *testing.T) {
+	assert.Panicsf(t, func() { ConvertString("someName", "{") }, "Expecting panic")
+}
