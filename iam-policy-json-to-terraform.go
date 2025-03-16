25fa0fccc9539c33eb323207707ed0331d3aa603
Florian Sellmayr <florian.sellmayr@gmail.com>
2025-03-16 15:11:26 +0100

    Release 1.9.1: Update AppVersion constant

--- iam-policy-json-to-terraform.go
+++ iam-policy-json-to-terraform.go
@@ -12,7 +12,7 @@ import (
 )
 
 // AppVersion : current version
-const AppVersion = "1.9.0"
+const AppVersion = "1.9.1"
 
 func main() {
 	policyName := flag.String("name", "policy", "name of the policy in generated hcl")
